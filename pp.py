89075eba36bb851ccd71232294186eabf336b5bb
niels.smet <smet.niels@provilion.be>
2026-03-17 13:36:59 +0100

    uydc

--- pp.py
+++ pp.py
@@ -7,10 +7,24 @@ pygame.init()
 running = True
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Pong')
-
+x = 200
+y = 200
 #Game loop begins
 while True:
-    pygame.draw.circle(screen, (255, 144, 45), (300, 200), 50)
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < 500 - width:
+        x += vel
+
+    if keys[pygame.K_UP] and y > 0:
+        y -= vel
+
+    if keys[pygame.K_DOWN] and y < 500 - height:
+        y += vel
+    pygame.draw.circle(screen, (255, 144, 0), (x, y), 50)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
