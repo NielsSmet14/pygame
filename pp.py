5a015648e2dc1b26824c9be35545cf4680bbaaa9
niels.smet <smet.niels@provilion.be>
2026-03-17 13:05:42 +0100

    uydc

--- pp.py
+++ pp.py
@@ -1,9 +1,18 @@
 import pygame
+import sys
+from pygame.locals import *
 
 
 pygame.init()
-
+running = True
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Pong')
-for e in range(42323):
+
+#Game loop begins
+while True:
     pygame.draw.circle(screen, (255, 34, 45), (300, 200), 50)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
