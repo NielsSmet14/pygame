b8a2ac436cea89ab6b5a99a6037a8d38137ecdff
niels.smet <smet.niels@provilion.be>
2026-03-17 14:13:04 +0100

    delete functie toegevoegd

--- pp.py
+++ pp.py
@@ -2,18 +2,20 @@ import pygame
 import sys
 from pygame.locals import *
 
-#start game
-pygame.init()
-running = True
-screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption('Pong')
-
 #variabelen
 x = 200
 y = 200
 vel = 1
-width = 20
-height = 20
+radius = 5
+reswidth=1280
+resheight=720
+
+#start game
+pygame.init()
+running = True
+
+screen = pygame.display.set_mode((reswidth, resheight))
+pygame.display.set_caption('Pong')
 
 #Game loop begins
 while True:
@@ -21,25 +23,27 @@ while True:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] and x > 0:
-        pygame.draw.circle(screen, (255, 0, 0), (x, y), 50)
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), radius)
         x -= vel
-        pygame.draw.circle(screen, (255, 144, 0), (x, y), 50)
+        pygame.draw.circle(screen, (255, 144, 0), (x, y), radius)
 
-    if keys[pygame.K_RIGHT] and x < 1280 - width:
-        pygame.draw.circle(screen, (255, 0, 0), (x, y), 50)
+    if keys[pygame.K_RIGHT] and x < 1280 - radius:
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), radius)
         x += vel
-        pygame.draw.circle(screen, (255, 144, 0), (x, y), 50)
+        pygame.draw.circle(screen, (255, 144, 0), (x, y), radius)
 
     if keys[pygame.K_UP] and y > 0:
-        pygame.draw.circle(screen, (255, 0, 0), (x, y), 50)
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), radius)
         y -= vel
-        pygame.draw.circle(screen, (255, 144, 0), (x, y), 50)
+        pygame.draw.circle(screen, (255, 144, 0), (x, y), radius)
 
-    if keys[pygame.K_DOWN] and y < 720 - height:
-        pygame.draw.circle(screen, (255, 0, 0), (x, y), 50)
+    if keys[pygame.K_DOWN] and y < 720 - radius:
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), radius)
         y += vel
-        pygame.draw.circle(screen, (255, 144, 0), (x, y), 50)
-
+        pygame.draw.circle(screen, (255, 144, 0), (x, y), radius)
+    if keys[pygame.K_SPACE]:
+        pygame.draw.rect(screen, (0, 0, 0), (0, 0,reswidth,resheight))
+        pygame.draw.circle(screen, (255, 144, 0), (x, y), radius)
     #exitgamedinges
     for event in pygame.event.get():
         if event.type == QUIT:
