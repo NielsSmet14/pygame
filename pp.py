932ed2545969296e2aa672f5e1a1a620aca624bb
niels.smet <smet.niels@provilion.be>
2026-03-17 12:51:50 +0100

    uydc

--- pp.py
+++ pp.py
@@ -5,5 +5,5 @@ pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Pong')
-
-pygame.draw.circle(screen, (255, 0, 0), (300, 200), 50)
+for e in range(42323):
+    pygame.draw.circle(screen, (255, 34, 45), (300, 200), 50)
